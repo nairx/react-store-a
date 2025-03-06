e0ef83142fc1a6f38a6b66c099ad9e2a82f6e861
nairx <nairxprv@gmail.com>
2025-03-06 13:37:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'; 
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <Header/>  
+    <div>
+      <Header />
+      <Products />
+      <Footer/>
+    </div>
   );
 }
 export default App;
