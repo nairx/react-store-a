f4b46991df198eb78370f480a4d4c44796b96027
nairx <nairxprv@gmail.com>
2025-03-06 14:46:34 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,9 @@ export default function Products() {
     { id: 1, name: "Product 1", price: 30 },
     { id: 2, name: "Product 2", price: 40 },
     { id: 3, name: "Product 3", price: 45 },
+    { id: 4, name: "Product 4", price: 95 },
+    { id: 5, name: "Product 5", price: 70 },
+    { id: 6, name: "Product 6", price: 25 },
   ];
   return (
     <div>
@@ -12,6 +15,7 @@ export default function Products() {
           <div className="App-Products-Box" key={index}>
             <h3>{value.name}</h3>
             <h4>{value.price}</h4>
+            <button>Add to Cart</button>
             </div>
         ))}
       </div>
