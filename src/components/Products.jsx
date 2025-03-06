eabd8e4419555fdb0dc3b2652d0f5c1cb55bc6f4
nairx <nairxprv@gmail.com>
2025-03-06 14:11:48 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,20 @@
-export default function Products(){
-    return(
-        <div>
-            <h2>Product List</h2>
-        </div>
-    )
+import "./Products.css"
+export default function Products() {
+  const products = [
+    { id: 1, name: "Product 1", price: 30 },
+    { id: 2, name: "Product 2", price: 40 },
+    { id: 3, name: "Product 3", price: 45 },
+  ];
+  return (
+    <div>
+      <div className="App-Products-Row">
+        {products.map((value, index) => (
+          <div className="App-Products-Box" key={index}>
+            <h3>{value.name}</h3>
+            <h4>{value.price}</h4>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
 }
