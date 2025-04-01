a0d11f002b464af07a3b16f943052d6500d0fee3
nairx <nairxprv@gmail.com>
2025-04-01 14:58:57 +0530

    Updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,9 +3,21 @@ import { appContext } from "../App";
 import { useContext } from "react";
 export default function Cart() {
   const { cart, setCart, products } = useContext(appContext);
+  const handleDelete = (id) => {
+    setCart({ ...cart, [id]: 0 });
+  };
   return (
     <div>
-      {products.map((value) => cart[value.id] && <div>{value.name}-{value.price}-{cart[value.id]}-{value.price*cart[value.id]}</div>)}
+      {products.map(
+        (value) =>
+          cart[value.id]>0 && (
+            <div key={value.id}>
+              {value.name}-{value.price}-{cart[value.id]}-
+              {value.price * cart[value.id]}-
+              <button onClick={() => handleDelete(value.id)}>Delete</button>
+            </div>
+          )
+      )}
     </div>
   );
 }
