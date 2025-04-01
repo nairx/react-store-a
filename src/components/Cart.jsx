7c35cc79e0280a78da13d4af0f816d4eefa65f74
nairx <nairxprv@gmail.com>
2025-04-01 14:46:35 +0530

    Updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { appContext } from "../App";
+import { useContext } from "react";
 export default function Cart() {
+  const { cart, setCart, products } = useContext(appContext);
   return (
-    <div>Cart</div>
-  )
+    <div>
+      {products.map((value) => cart[value.id] && <div>{value.name}-{value.price}-{cart[value.id]}-{value.price*cart[value.id]}</div>)}
+    </div>
+  );
 }
